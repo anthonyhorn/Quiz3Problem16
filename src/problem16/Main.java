bffe171f7e4058d01aadd657e231f76751f08b20
anthonyhorn <ajhorn@udel.edu>
2015-03-29 14:41:43 -0400

    Problem 16

---
+++ src/problem16/Main.java
@@ -0,0 +1,12 @@
+package problem16;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Triangle a = new Triangle(5,6,7.9);
+		System.out.println(a.getArea());
+		System.out.println(a.getPerimeter());
+		System.out.println(a.toString());
+	}
+
+}
---
+++ src/problem16/Triangle.java
@@ -0,0 +1,50 @@
+package problem16;
+
+public class Triangle{
+	
+	double side1=1.0;
+	double side2=1.0;
+	double side3=1.0;
+
+	public Triangle(){
+		
+		this.side1=1.0;
+		this.side2=1.0;
+		this.side3=1.0;
+		
+	}
+	
+	public Triangle(double side1, double side2, double side3){
+		
+		this.side1=side1;
+		this.side2=side2;
+		this.side3=side3;
+		
+	}
+	
+	public double getSide1(){
+		return this.side1;
+	}
+	
+	public double getSide2(){
+		return this.side2;
+	}
+	
+	public double getSide3(){
+		return this.side3;
+	}
+	
+	public double getArea(){
+		double halfPerimeter=((this.side1+this.side2+this.side3)/2);
+		return Math.pow((halfPerimeter*(halfPerimeter-this.side1)*(halfPerimeter-this.side2)*(halfPerimeter-this.side3)),.5);
+	}
+	
+	public double getPerimeter(){
+		return this.side1+this.side2+this.side3;
+	}
+	
+	public String toString(){
+		return getClass().getName() + '@' + Integer.toHexString(hashCode())
+ + "-side1="+this.getSide1()+ "-side2="+this.getSide2()+ "-side3="+this.getSide3();
+	}
+}
